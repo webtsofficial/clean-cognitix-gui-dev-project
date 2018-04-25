72680985e781e44e3400853bdf8d0844e6d7b12f
Tobias Schramek <webtsofficial@gmail.com>
2018-04-25 23:39:37 +0200

    added getAlbums() Method to AlbumsService and added Album Model Class to project

--- src/app/services/albums/albums.service.spec.ts
+++ src/app/services/albums/albums.service.spec.ts
@@ -1,15 +1,58 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {
+    TestBed,
+    inject,
+    getTestBed
+} from '@angular/core/testing';
 
-import { AlbumsService } from './albums.service';
+import {AlbumsService} from './albums.service';
+import {
+    HttpClientTestingModule,
+    HttpTestingController
+} from '@angular/common/http/testing';
+import {APIURL} from '../../app.module';
+import {Album} from '../../models/Album';
 
 describe('AlbumsService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AlbumsService]
-    });
-  });
+    let injector: TestBed,
+        service: AlbumsService,
+        httpMock: HttpTestingController;
 
-  it('should be created', inject([AlbumsService], (service: AlbumsService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [AlbumsService]
+        });
+
+        injector = getTestBed();
+        service = injector.get(AlbumsService);
+        httpMock = injector.get(HttpTestingController);
+    });
+
+    it('should be created', inject([AlbumsService], () => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('getAlbums() : Observable Album[]', inject([AlbumsService], () => {
+        const dummyAlbums: Album[] = [
+            {
+                'userId': 1,
+                'id': 1,
+                'title': 'quidem molestiae enim'
+            },
+            {
+                'userId': 1,
+                'id': 2,
+                'title': 'sunt qui excepturi placeat culpa'
+            }
+        ];
+
+        service.getAlbums().subscribe(albums => {
+            expect(albums.length).toBe(2);
+            expect(albums).toEqual(dummyAlbums);
+        });
+
+        const req = httpMock.expectOne(APIURL + '/albums/');
+        expect(req.request.method).toBe('GET');
+        req.flush(dummyAlbums);
+    }));
 });
--- src/app/services/albums/albums.service.ts
+++ src/app/services/albums/albums.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import {Album} from '../../models/Album';
+import {APIURL} from '../../app.module';
 
 @Injectable()
 export class AlbumsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAlbums(): Observable<Album[]> {
+    const url = APIURL + '/albums/';
+    return this.http.get<Album[]>(url).map(
+        albums => albums as Album[]
+    );
+  }
 
 }
