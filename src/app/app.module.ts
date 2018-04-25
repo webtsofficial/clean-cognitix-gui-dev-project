d1c95070e3557427297641ab1d4ecb547f4d1227
Tobias Schramek <webtsofficial@gmail.com>
2018-04-25 19:29:34 +0200

    added project dependencies and deleted basic project template - linting ok - testing ok
    #dependencies:
    ajv@^6.4.0
    bootstrap@^4.1.0 + src in angular-cli.json
    jquery@^3.3.1 + src in angular-cli.json
    ng-lazyload-image@^3.4.2 + import in AppModule
    popper.js@^1.14.3 + src in angular-cli.json
    Google Fonts - link in index.html
    Fontawesome - link in index.html
    
    made unit tests and e2e tests working
    checked linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// Modules
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {LazyLoadImageModule} from 'ng-lazyload-image';
+// Components
+import {AppComponent} from './app.component';
+// Services
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-
+// Pipes
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        LazyLoadImageModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
