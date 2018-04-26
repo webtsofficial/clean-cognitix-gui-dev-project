49c64ab4835accf8378c5605c47d05554d1d92d9
Tobias Schramek <webtsofficial@gmail.com>
2018-04-27 01:31:48 +0200

    added constructor to user class Model

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -10,4 +10,15 @@ export class User {
     phone: string;
     website: string;
     company: Company;
+
+    constructor(userObj) {
+        this.id = userObj.id;
+        this.name = userObj.name;
+        this.username = userObj.username;
+        this.email = userObj.email;
+        this.address = userObj.address;
+        this.phone = userObj.phone;
+        this.website = userObj.website;
+        this.company = userObj.company;
+    }
 }
