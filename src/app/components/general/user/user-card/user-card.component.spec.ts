4bdfdf64a6580d6176b365c3e1cbe19ff10fa704
Tobias Schramek <webtsofficial@gmail.com>
2018-04-27 01:34:01 +0200

    userCardComponent added @Input user and @Output activeUser + changeActiveUserfunction: EventEmitter + Test

--- src/app/components/general/user/user-card/user-card.component.spec.ts
+++ src/app/components/general/user/user-card/user-card.component.spec.ts
@@ -1,25 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+    async,
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
 
-import { UserCardComponent } from './user-card.component';
+import {UserCardComponent} from './user-card.component';
+import {User} from '../../../../models/User';
 
 describe('UserCardComponent', () => {
-  let component: UserCardComponent;
-  let fixture: ComponentFixture<UserCardComponent>;
+    let component: UserCardComponent;
+    let fixture: ComponentFixture<UserCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserCardComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [UserCardComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('changeActiveUser() shoultd emit User object', () => {
+
+        const userObj = {
+                'id': 1,
+                'name': 'Leanne Graham',
+                'username': 'Bret',
+                'email': 'Sincere@april.biz',
+                'address': {
+                    'street': 'Kulas Light',
+                    'suite': 'Apt. 556',
+                    'city': 'Gwenborough',
+                    'zipcode': '92998-3874',
+                    'geo': {
+                        'lat': '-37.3159',
+                        'lng': '81.1496'
+                    }
+                },
+                'phone': '1-770-736-8031 x56442',
+                'website': 'hildegard.org',
+                'company': {
+                    'name': 'Romaguera-Crona',
+                    'catchPhrase': 'Multi-layered client-server neural-net',
+                    'bs': 'harness real-time e-markets'
+                }
+            },
+            dummyUser: User = new User(userObj);
+
+        component.activeUser.subscribe(activeUser => {
+            expect(activeUser).toEqual(userObj);
+        });
+        component.changeActiveUser(userObj);
+    });
 });
--- src/app/components/general/user/user-card/user-card.component.ts
+++ src/app/components/general/user/user-card/user-card.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output
+} from '@angular/core';
+import {User} from '../../../../models/User';
+
 
 @Component({
-  selector: 'app-user-card',
-  templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+    selector: 'app-user-card',
+    templateUrl: './user-card.component.html',
+    styleUrls: ['./user-card.component.scss']
 })
 export class UserCardComponent implements OnInit {
 
-  constructor() { }
+    @Input() user: User;
+    @Output() activeUser: EventEmitter<User> = new EventEmitter<User>();
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    changeActiveUser(user: User): void {
+        this.activeUser.emit(user);
+    }
 
 }
