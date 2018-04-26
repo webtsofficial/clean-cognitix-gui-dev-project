0b117927f00f7c14c31284b5f9e5f28af6ad7241
Tobias Schramek <webtsofficial@gmail.com>
2018-04-26 08:13:42 +0200

    created Photo Model Class and getPhotos() in PhotosServie + Test

--- src/app/services/photos/photos.service.spec.ts
+++ src/app/services/photos/photos.service.spec.ts
@@ -9,6 +9,8 @@ import {
     HttpClientTestingModule,
     HttpTestingController
 } from '@angular/common/http/testing';
+import {APIURL} from '../../app.module';
+import {Photo} from '../../models/Photo';
 
 describe('PhotosService', () => {
     let injector: TestBed,
@@ -32,5 +34,47 @@ describe('PhotosService', () => {
 
     it('should be created', inject([PhotosService], () => {
         expect(service).toBeTruthy();
+    }));
+
+    it('getAlbums() : Observable Album[]', inject([PhotosService], () => {
+        const dummyPhotos: Photo[] = [
+            {
+            'albumId': 1,
+            'id': 49,
+            'title': 'quasi quae est modi quis quam in impedit',
+            'url': 'http://placehold.it/600/2cd88b',
+            'thumbnailUrl': 'http://placehold.it/150/2cd88b'
+            },
+            {
+                'albumId': 1,
+                'id': 50,
+                'title': 'et inventore quae ut tempore eius voluptatum',
+                'url': 'http://placehold.it/600/9e59da',
+                'thumbnailUrl': 'http://placehold.it/150/9e59da'
+            },
+            {
+                'albumId': 2,
+                'id': 51,
+                'title': 'non sunt voluptatem placeat consequuntur rem incidunt',
+                'url': 'http://placehold.it/600/8e973b',
+                'thumbnailUrl': 'http://placehold.it/150/8e973b'
+            },
+            {
+                'albumId': 2,
+                'id': 52,
+                'title': 'eveniet pariatur quia nobis reiciendis laboriosam ea',
+                'url': 'http://placehold.it/600/121fa4',
+                'thumbnailUrl': 'http://placehold.it/150/121fa4'
+            }
+        ];
+
+        service.getPhotos().subscribe(photos => {
+            expect(photos.length).toBe(4);
+            expect(photos).toEqual(dummyPhotos);
+        });
+
+        const req = httpMock.expectOne(APIURL + '/photos/');
+        expect(req.request.method).toBe('GET');
+        req.flush(dummyPhotos);
     }));
 });
--- src/app/services/photos/photos.service.ts
+++ src/app/services/photos/photos.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import {APIURL} from '../../app.module';
+import {Photo} from '../../models/Photo';
 
 @Injectable()
 export class PhotosService {
 
   constructor(private http: HttpClient) { }
+
+  getPhotos(): Observable<Photo[]> {
+    const url = APIURL + '/photos/';
+    return this.http.get<Photo[]>(url).map(
+        photos => photos as Photo[]
+    );
+  }
 
 }
